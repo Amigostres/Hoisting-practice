0509a2bddbfc3bed9b3c5f3b877c8920fc607691
Jose <josemanuel21302@gmail.com>
2023-02-14 03:17:45 -0800

    fully understand hoisting

---
+++ main.js
@@ -0,0 +1,58 @@
+
+
+
+
+// const addTwo = (number) => {
+//     number += 2
+//     console.log(number)
+// }
+
+// addTwo(2)
+
+//======================================
+
+
+// x = 5; // Assign 5 to x
+// //this will look to see if x is declared and it will see a few lines down
+// //when it does it will see (var x;) and it will bring it to the top and (var x) is equal to undefine
+// //so when the declaration move to the top it will leave the initialization at the bottom because it ignores it and goes top to bottom
+// //
+
+// elem = document.getElementById("demo"); // Find an element
+// elem.innerHTML = x;                     // Display x in the element
+
+// //the (var x )will go to the top and act as a declaclararion and leave it
+// var x = 2; // Declare x
+
+
+
+//======================================
+
+
+// var x = 5 // declares and Initializes x
+
+// elem = document.getElementById("demo")  // Find an element names demo
+// elem.innerHTML = x + " " + y            // Displays the content `5 undefined`
+//                                         //however it is going to be undefine because it will get the (var y;)
+//                                         //and (var y) is equal to undefine
+//                                         //remember that it ignores the initialization and brings up (var y)
+
+// var y = 7 // Initializes and declares y
+
+// //this example will show how the declareation will bring up but not the initialization (=7)
+
+//------------------the example below is the same in the top---------------------
+
+// var x = 5
+// var y    // Declare y
+
+// elem = document.getElementById("demo")
+// elem.innerHTML = x + " " + y           
+
+// y = 7    // Assign 7 to y
+
+
+
+
+//======================================
+
